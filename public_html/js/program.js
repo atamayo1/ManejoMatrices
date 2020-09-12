c441212af41ea30c0a39597026b54df83bad449e
atamayo1 <anthony1_144@hotmail.com>
2020-09-11 20:36:51 -0500

    i upload all the changes on the project

---
+++ public_html/js/program.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function manejoMatrices(){
+    let arr1 = ['a','b','c'];
+    let arr2 = ['d','e','f'];
+    let arr3 = ['g','h','i'];
+    
+    /* Una matriz es un arreglo de arreglos */
+    let matriz = [arr1, arr2, arr3];
+    
+    /* Ciclos anidados */
+    // Recorrer Matriz
+    for (var i = 0; i < matriz.length; i++) {
+        // Recorrer cada arreglo individual
+        for (var j = 0; j < matriz[i].length; j++) {
+            
+        }
+    }
+}
